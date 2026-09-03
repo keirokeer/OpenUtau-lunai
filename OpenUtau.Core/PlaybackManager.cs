861910b3a013bd0ec84be00fe3bc395d5537397f
cadlaxa <92255161+Cadlaxa@users.noreply.github.com>
2026-09-03 12:49:37 +0800

    Fix Minor Bugs (#2148)
    * Fix minor bugs
    
    * Fix phoneme error with a new way to not obstruct the phoneme override
    
    * Fix brush color on highlighted error notes
    
    * Fix expression shadow + UI tweaks
    
    * Fix PlayTone not turning off + revamped phonemizer tag
    
    * Change default value for detached pianoroll
    
    * revert changes
    
    * adjust numerical fillOpacity
    
    * fix strings
    
    * fix duplicate strings

--- OpenUtau.Core/PlaybackManager.cs
+++ OpenUtau.Core/PlaybackManager.cs
@@ -309,6 +309,9 @@ namespace OpenUtau.Core {
         }
 
         public void PlayTone(double freq) {
+            if (!Core.Util.Preferences.Default.PlayTone) {
+                return;
+            }
             toneGenerator.StartTone(freq);
 
             // If nothing is playing, start editing mix
