401e1d9f6210199622c2ca3ecf1ad8b04b5582ca
keirokeer <83983873+keirokeer@users.noreply.github.com>
2026-09-05 16:23:48 +0200

    fix: play piano-roll keyboard tones when Play Tone is off
    Play Tone still gates note-edit previews; keyboard clicks always audition.

--- OpenUtau.Core/PlaybackManager.cs
+++ OpenUtau.Core/PlaybackManager.cs
@@ -309,9 +309,6 @@ namespace OpenUtau.Core {
         }
 
         public void PlayTone(double freq) {
-            if (!Core.Util.Preferences.Default.PlayTone) {
-                return;
-            }
             toneGenerator.StartTone(freq);
 
             // If nothing is playing, start editing mix
