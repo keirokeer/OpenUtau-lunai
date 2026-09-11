c1c12233a07cac4c282fbefa191a0f742fa06f6e
keirokeer <83983873+keirokeer@users.noreply.github.com>
2026-09-11 18:29:24 +0200

    fix: realign EnToJa/EnVCCV goldens after SBP merge; polish Wayland row
    SBP EnablePhonemeTokenization changed nasal/cluster output; restore expectations to match. Layout Linux Wayland toggle with two columns.

--- OpenUtau.Test/Plugins/EnVCCVTest.cs
+++ OpenUtau.Test/Plugins/EnVCCVTest.cs
@@ -114,7 +114,7 @@ namespace OpenUtau.Plugins {
                         },
                     }
                 }
-            }, new string[] { "-hhI", "I-_H" });
+            }, new string[] { "-hI", "I-_H" });
         }
 
         [Theory]
